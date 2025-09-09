627f06290a1c7ea5d65f532ff72c9a1c10a8787b
Grandil <4243333+Grandil@users.noreply.github.com>
2025-09-09 08:53:40 -0700

    Project after assignment CH2 L5

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,14 +9,21 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0 #delta_time
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        screen.fill(000)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit game to 60 FPS
+        dt = clock.tick(60)/1000
 
 
 
